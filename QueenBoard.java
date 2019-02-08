6c033e9bd46cab6e738ec6c5a8f936e8ec30e412
cohyunoh <coh00@stuy.edu>
2019-02-07 22:44:56 -0500

    addqueen worked

--- QueenBoard.java
+++ QueenBoard.java
@@ -8,13 +8,13 @@ public class QueenBoard{
   public boolean addQueen(int r, int c){
     if((r>=0 || r < board.length) && (c>=0 || c < board.length)){
       board[r][c] = -1;
-      for(int i = 0; r + i < board.length && c + i < board.length; i++){
+      for(int i = 1; r + i < board.length && c + i < board.length; i++){
         board[r + i][c + i] += 1;
       }
-      for(int i = 0; c + i < board.length && r - i >= 0; i++){
+      for(int i = 1; c + i < board.length && r - i >= 0; i++){
         board[r - i][c + i] += 1;
       }
-      for(int i = 0; c + i < board.length; i++){
+      for(int i = 1; c + i < board.length; i++){
         board[r][c + i] += 1;
       }
       return true;
@@ -25,13 +25,13 @@ public class QueenBoard{
   public boolean removeQueen(int r, int c){
     if((r>=0 || r < board.length) && (c>=0 || c < board.length)){
       board[r][c] = 0;
-      for(int i = 0; r + i < board.length && c + i < board.length; i++){
+      for(int i = 1; r + i < board.length && c + i < board.length; i++){
         board[r + i][c + i] -= 1;
       }
-      for(int i = 0; c + i < board.length && r - i >= 0; i++){
+      for(int i = 1; c + i < board.length && r - i >= 0; i++){
         board[r - i][c + i] -= 1;
       }
-      for(int i = 0; c + i < board.length; i++){
+      for(int i = 1; c + i < board.length; i++){
         board[r][c + i] -= 1;
       }
       return true;
