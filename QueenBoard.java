4946c4b793948381f510f0cb552d06aa7f554602
cohyunoh <coh00@stuy.edu>
2019-02-07 22:33:59 -0500

    coded first version of remove queen

--- QueenBoard.java
+++ QueenBoard.java
@@ -22,5 +22,20 @@ public class QueenBoard{
     return false;
   }
 
-  private boolean removeQueen
+  private boolean removeQueen(int r, int c){
+    if((r>=0 || r < board.length) && (c>=0 || c < board.length)){
+      board[r][c] = 0;
+      for(int i = 0; r + i < board.length && c + i < board.length; i++){
+        board[r + i][c + i] -= 1;
+      }
+      for(int i = 0; c + i < board.length && r - i >= 0; i++){
+        board[r - i][c + i] -= 1;
+      }
+      for(int i = 0; c + i < board.length; i++){
+        board[r][c + i] -= 1;
+      }
+      return true;
+    }
+    return false;
+  }
 }
