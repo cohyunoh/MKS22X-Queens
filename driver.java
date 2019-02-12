054458439f7832f8cf3ab06198b9e37a5d3aaabd
cohyunoh <coh00@stuy.edu>
2019-02-11 19:29:24 -0500

    tried to test 100 boards. Was taking too long

--- driver.java
+++ driver.java
@@ -1,7 +1,7 @@
 public class driver{
   public static void main(String[] args) {
     try{
-      for(int i = 0; i < 10; i++){
+      for(int i = 0; i < 100; i++){
         QueenBoard board = new QueenBoard(i);
         System.out.println(board.solve());
         System.out.println(board);
